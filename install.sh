3e58c04f3fb3bea8e51574cd56da8ed2d7ac3647
Andrew <andrew@lobos.me>
2013-08-03 23:06:03 -0400

    Added support for updating from a previous version Added documentation for updating

--- install.sh
+++ install.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
 
-adduser --system --shell /bin/bash --group --gecos 'User for PaperPlane webapp' --disabled-password --home /home/paperplane paperplane
-cp upstart.conf /etc/init/paperplane.conf
-cp -R . /var/local/paperplane
-touch /var/log/paperplane.log
-chown paperplane:paperplane /var/log/paperplane.log
-cd /var/local/paperplane; npm link
-chown -R paperplane:paperplane /var/local/paperplane
-start paperplane
+if [ -d "/var/local/paperplane" ]; then
+	echo "Updating existing install"
+	stop paperplane
+	cp -R . /var/local/paperplane
+	chown -R paperplane:paperplane /var/local/paperplane
+	start paperplane
+fi
+
+if [ ! -d "/var/local/paperplane" ]; then
+	# Add user
+	adduser --system --group --gecos 'User for PaperPlane webapp' --disabled-password --home /home/paperplane paperplane
+	
+	# Copy upstart config
+	cp upstart.conf /etc/init/paperplane.conf
+	
+	# Copy application folder and create log file
+	cp -R . /var/local/paperplane
+	touch /var/log/paperplane.log
+	chown paperplane:paperplane /var/log/paperplane.log
+	
+	# Install node deps
+	cd /var/local/paperplane; npm link
+	chown -R paperplane:paperplane /var/local/paperplane
+	
+	start paperplane
+fi
